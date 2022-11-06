02ca0f49161e1c784be117605020d8c89e4c56bd
IWonderKanyeWest <117598066+IWonderKanyeWest@users.noreply.github.com>
2022-11-06 17:00:50 -0600

    Delete First Person (Put in server script storage).lua

--- First Person (Put in server script storage).lua
+++
@@ -1,7 +0,0 @@
--- Services
-local playerService = game:GetService("Players")
-
-playerService.PlayerAdded:Connect(function (plr)
-	plr.CharacterAdded:wait()
-	plr.CameraMode = Enum.CameraMode.LockFirstPerson
-end)
